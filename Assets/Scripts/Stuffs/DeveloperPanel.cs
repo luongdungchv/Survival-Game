8d5728e0267e5825881b519e6362250e05da1959
lonewolf2 <50980440+luongdungchv@users.noreply.github.com>
2023-02-27 01:57:53 +0700

    add: developer command

---
+++ Assets/Scripts/Stuffs/DeveloperPanel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeveloperPanel : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField inputField;
+    private void Start() {
+        inputField.onSubmit.AddListener((s) => {
+            var command = inputField.text;
+            inputField.text = "";
+            if(command.Length == 0) return;
+            var split = command.Split(' ');
+            if(split[0] == "add"){
+                Inventory.ins.Add(split[1], int.Parse(split[2]));
+            }
+            else if(split[0] == "regen_hp"){
+                NetworkPlayer.localPlayer.GetComponent<PlayerStats>().RegenerateHP(float.Parse(split[1]));
+            }
+            else if(split[0] == "regen_hunger"){
+                NetworkPlayer.localPlayer.GetComponent<PlayerStats>().RegenerateHungerPoint(float.Parse(split[1]));
+            }
+            else if(split[0] == "regen_stamina"){
+                NetworkPlayer.localPlayer.GetComponent<PlayerStats>().RegenerateStamina(float.Parse(split[1]));
+            }
+            else if(split[0] == "invincible"){
+                if(split[1] == "on") NetworkPlayer.localPlayer.GetComponent<PlayerStats>().SetInvincible(true);
+                else if(split[1] == "off") NetworkPlayer.localPlayer.GetComponent<PlayerStats>().SetInvincible(false);
+            }
+        });
+    }
+    private void Update() {
+        if(inputField.isFocused && Input.GetKeyDown(KeyCode.Return)){
+            
+        }
+    }
+}
